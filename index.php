4798c0a538e4a264eec222f6b9f50e42521b640c
IKSWEB <46905310+iks-web@users.noreply.github.com>
2021-08-12 21:43:24 +0300

    Update index.php
    Update file php 8.0

--- index.php
+++ index.php
@@ -3,22 +3,24 @@
  * Pure-PHP PKCS#1 compliant implementation of RSA.
  *
  * @author  Sergey Knyazew <info@iksweb.ru>
- * @version 0.1.0
+ * @version 0.1.1
  * @access  public
  * @package Crypt_RSA
  */
 
 // Загружаем класс обработчик
-include_require($_SERVER['DOCUMENT_ROOT'].'/include/CryptRSA.php');
+require(__DIR__.'/include/CryptRSA.php');
 $rsa = new Crypt_RSA;
 
-print_r($rsa->createKey()); // Создаём ключи (необходимо скопировать в файлы openssl_private.txt и openssl_publick.txt)
+$rsa->ShowKey();  // Создаём ключи (необходимо скопировать в файлы openssl_private.txt и openssl_publick.txt)
+
 
 // Шифруем текст по ключу
-$rsa->loadKey('openssl_publick.txt'); // load public key
-$contentsEncrypted = $rsa->encrypt('Текст для кодировки');
+$rsa->LoadKey(__DIR__.'/rsa/openssl_publick.txt'); // load public key
+$contentsEncrypted = $rsa->Encrypt('Текст для кодировки');
+
 
 // Производим дешифрацию
-$rsa->loadKey('openssl_private.txt'); // load public key
-echo $rsa->decrypt($contentsEncrypted);
+$rsa->LoadKey(__DIR__.'/rsa/openssl_private.txt'); // load public key
+echo $rsa->Decrypt($contentsEncrypted);
 ?>
